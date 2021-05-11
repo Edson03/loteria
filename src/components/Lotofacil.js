a0905b26d80dc2fa8bd115a71ebdb626aca0bec0
edson.mateus <edsonmateusmc@gmail.com>
2021-05-11 01:02:15 -0300

    added formik library

--- src/components/Lotofacil.js
+++ src/components/Lotofacil.js
@@ -1,25 +1,54 @@
-import { Box, SimpleGrid, Center, Checkbox, Flex, Button } from "@chakra-ui/react"
+import { useState} from 'react'
+import { Box, SimpleGrid, Center, CheckboxGroup, Checkbox, Flex, Button } from "@chakra-ui/react"
+
+import { useFormik } from 'formik'
+
+
+const resultado_fixar = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25']
+const soma = ['184 à 199', '200 à 215', '168 à 183', '216 à 231', '152 à 167', '232 à 247', '136 à 151', '120 à 135', '264 à 270', '248 à 263']
+const linha = [43332, 44322, 44331, 53322, 54321, 44421, 33333, 54222, 53331, 44430, 54411, 54330, 54420, 55221, 55311, 55320, 55500, 55410]
+const coluna = [43332, 44322, 44331, 53322, 54321, 44421, 33333, 54222, 53331, 54330, 54411, 55221, 54420, 44430, 55320, 55311, 55500, 55410]
+const par_impar = ['7 8', '8 7', '6 9', '9 6', '5 10', '10 5', '4 11', '11 4', '3 12', '2 13', '12 3']
+const repetido = ['05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15']
+const primo_cruz = ['01', '02', '03', '04', '05', '06', '07', '08', '09']
+const fibonacci = ['01', '02', '03', '04', '05', '06', '07']
+const xis = ['02', '03', '04', '05', '06', '07', '08', '09']
+
+const validate = values => {
+    const errors = {};
+  
+    if (!values.firstName) {
+      errors.firstName = 'Required';
+    } else if (values.firstName.length > 15) {
+      errors.firstName = 'Must be 15 characters or less';
+    }
+  
+    return errors;
+};
+
 
 const Lotofacil = () => {
 
-    const resultado_fixar = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25']
-    const soma = ['184 à 199', '200 à 215', '168 à 183', '216 à 231', '152 à 167', '232 à 247', '136 à 151', '120 à 135', '264 à 270', '248 à 263']
-    const linha = [43332, 44322, 44331, 53322, 54321, 44421, 33333, 54222, 53331, 44430, 54411, 54330, 54420, 55221, 55311, 55320, 55500, 55410]
-    const coluna = [43332, 44322, 44331, 53322, 54321, 44421, 33333, 54222, 53331, 54330, 54411, 55221, 54420, 44430, 55320, 55311, 55500, 55410]
-    const par_impar = ['7 8', '8 7', '6 9', '9 6', '5 10', '10 5', '4 11', '11 4', '3 12', '2 13', '12 3']
-    const repetido = ['05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15']
-    const primo_cruz = ['01', '02', '03', '04', '05', '06', '07', '08', '09']
-    const fibonacci = ['01', '02', '03', '04', '05', '06', '07']
-    const xis = ['02', '03', '04', '05', '06', '07', '08', '09']
+    const formik = useFormik({
+        initialValues: {
+          resultado: []
+        },
+        onSubmit: values => {
+          alert(JSON.stringify(values, null, 2));
+        },
+    });
+
 
     return (
-
+        
         <Flex w="100%"  mt={5} mb={5} justify="center">
+            
             <Box
                 w={{ base: "100%", md: "100%", xl: "80%" }}
                 overflow="hidden"
                 justify="center"
             >
+                <form onSubmit={formik.handleSubmit}>
                 <Flex w="100%" justify="center">
                     <SimpleGrid columns={[1, 1, 2, 3]} rows="auto" spacing={5} >
                         <Box w="300px" >
@@ -47,11 +76,13 @@ const Lotofacil = () => {
                                 h="250px"
                             >
                                 <SimpleGrid p={4} columns={5} spacing={5}>
+                                    <CheckboxGroup colorScheme="purple" >
                                     {resultado_fixar.map((item, index) => (
-                                        <Checkbox key={index} colorScheme="green" defaultIsChecked>
+                                        <Checkbox key={index} name="resultado" value={item} onChange={formik.handleChange}>
                                             {item}
                                         </Checkbox>
                                     ))}
+                                    </CheckboxGroup>
                                 </SimpleGrid>
                             </Center>
                         </Box>
@@ -399,10 +430,15 @@ const Lotofacil = () => {
                 </Flex>
 
                 <Flex justify="center" mt={25} mb={5}>
-                    <Button>Gerar combinações</Button>
+                    <Button type="submit"> Gerar combinações</Button>
                 </Flex>
-            </Box>
+
+
+                </form>
+            </Box>                      
+            
         </Flex>
+        
     )
 }
 
